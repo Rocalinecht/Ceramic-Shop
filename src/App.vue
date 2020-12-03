3a8cb8950204a23f7a19a7a024d05bd462b784d3
Caroline <carolinechatelon@hotmail.fr>
2020-12-03 14:14:42 +0100

    make navbar ( router / bootstrap )

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div id="app">
-    <h1>Ceramic Shop</h1>
-    <Cart />
+
+    <div>
+      <b-navbar variant="faded" type="light" >
+        <b-navbar-brand tag="h1" class="mb-0">Ceramic Shop </b-navbar-brand>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item right><router-link to="/">Home </router-link> </b-nav-item>
+          <b-nav-item right><router-link to="/ceramic">Ceramic</router-link> </b-nav-item>
+          <b-nav-item right><router-link to="/about">About</router-link></b-nav-item>
+          <b-nav-item right><router-link to="/contact">Contact</router-link> </b-nav-item>    
+        </b-navbar-nav>
+      </b-navbar>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import Cart from './components/Cart.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Cart
-  },
- 
-}
-</script>
 
 <style>
 #app {
@@ -24,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
