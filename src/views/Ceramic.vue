2a08a1a46c2b725df67abbac049e6adf9dc032cb
Caroline <carolinechatelon@hotmail.fr>
2020-12-03 14:13:11 +0100

    create new vue ( Contact / Ceramic )

---
+++ src/views/Ceramic.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="ceramic">
+
+        <h1>This is an Ceramic page</h1>
+          
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <div class="box">
+      <h1>Ceramic Shop</h1>
+      <h2>Make with hands and Love</h2>
+</div>
+
+
+    <Cart/>
+</div>
+
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import Cart from '@/components/Cart.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Cart
+  }
+}
+</script>
+<style scoped>
+.box{
+  background-image: url('~@/assets/img/background.jpg');
+  background-attachment:fixed;
+  background-size: cover;
+  height: 550px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+h1{
+  color: white;
+  font-size: 3em;
+}
+h2{
+  font-style: italic;
+  color: white;
+}
+</style>
